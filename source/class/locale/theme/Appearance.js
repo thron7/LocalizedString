a1e89e2e2d230e689f6fece53f1fd26717ef7d1c
thron7 <thron7@users.sourceforge.net>
2013-02-02 21:37:08 +0100

    initial commit

---
+++ source/class/locale/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("locale.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ source/class/locale/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("locale.theme.Theme",
+{
+  meta :
+  {
+    color : locale.theme.Color,
+    decoration : locale.theme.Decoration,
+    font : locale.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : locale.theme.Appearance
+  }
+});
